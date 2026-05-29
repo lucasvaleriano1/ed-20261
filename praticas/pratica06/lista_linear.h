3ed30ad1f952ac516d9146e5b0b2cb3f5bb5ccd9
unknown <2512082011@ACADEMICO.IESB.BR>
2026-05-29 09:41:39 -0300

    feat: conclui pratica06. Fecha #ID

---
+++ praticas/pratica06/lista_linear.h
@@ -0,0 +1,12 @@
+#ifndef LISTA_LINEAR_H
+#define LISTA_LINEAR_H
+
+void lista_criar();
+void lista_inserir();
+void lista_remover();
+void lista_buscar();
+void lista_exibir();
+void lista_esta_vazia();
+void lista_destruir();
+
+#endif
---
+++ praticas/pratica06/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "lista_linear.h"
+
+int main(void) {
+    lista_criar();
+
+    return 0;
+}
